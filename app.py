77eec6dc27bd4aa0580ca333a514755ce5905df6
Berge Maxim <44107024+GraphtyLove@users.noreply.github.com>
2021-09-10 09:48:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import os
 
 # Generate a JWT for warcraft log that expire in 120 days.
 os.environ["WL_JWT"] = get_Oauth_jwt(os.environ["WL_CLIENT_ID"], os.environ["WL_CLIENT_SECRET"], os.environ["WL_AUTH_URL"])
+# Generate JWT for BattleNet API.
 os.environ["BN_JWT"] = get_Oauth_jwt(os.environ["BN_CLIENT_ID"], os.environ["BN_CLIENT_SECRET"], os.environ["BN_AUTH_URL"])
-print("BN ", os.environ["BN_JWT"])
 
 if __name__ == "__main__":
-    # Example
+    # Scrap top 100 players on a specific boss. Get dps or hps and player's stats.
     scrap_boss("Sanctum of Domination", "The Nine", "Shaman", "Elemental", "Night Fae", "Heroic")
 
 
